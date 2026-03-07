d0c6c08fc9b7d88af943d3b08ec7a2e7c191ed62
SARAVANAN SAIBABA <talk2sarvan@proton.me>
2026-03-07 13:32:12 -0500

    Example docs

---
+++ integers/adder.go
@@ -0,0 +1,5 @@
+package integers
+
+func Add(a, b int) (r int){
+	return a + b
+}
--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,2 +1,20 @@
 package integers
 
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T){
+	want := 5
+	got:= Add(3,2)
+	if got != want{
+		t.Errorf("Add failed want: %d got: %d", want, got)
+	}
+}
+
+func ExampleAdd() {
+		sum := Add(1, 5)
+		fmt.Println(sum)
+		// Output: 6
+}
