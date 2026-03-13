29c4a53993757bb0845581ba4a5328edb74c658d
SARAVANAN SAIBABA <talk2sarvan@proton.me>
2026-03-13 17:09:35 -0400

    Structs!

--- composites/composites.go
+++ composites/composites.go
@@ -1,14 +1,28 @@
 package composites
 
+import "math"
+
 type Rectangle struct {
 	length float64
 	width float64
 }
 
-func Area(r Rectangle) float64{
-	return r.length * r.width
+type Circle struct{
+	radius float64
 }
 
-func Perimeter(r Rectangle) float64 {
-	return 2 *(r.length + r.width)
+func (r Rectangle) Area() float64{
+	return math.Floor(r.length * r.width)
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return math.Floor(2 *(r.length + r.width))
+}
+
+func (c Circle) Area() float64 {
+	return math.Floor(math.Pi * (c.radius * c.radius))
+}
+
+func (c Circle) Perimeter() float64 {
+	return math.Floor(2 * math.Pi * c.radius)
 }
--- composites/composites_test.go
+++ composites/composites_test.go
@@ -4,18 +4,31 @@ import "testing"
 
 func TestArea(t *testing.T) {
   want := 50.0
-	got := Area(Rectangle{length:10.0, width:5.0})
+	got := Rectangle{length:10.0, width:5.0}.Area()
   assertResult(t, got, want)
 	
 }
 
 func TestPerimeter(t *testing.T) {
 	want := 30.0
-	got := Perimeter(Rectangle{length:10.0, width:5.0})
+	got := Rectangle{length:10.0, width:5.0}.Perimeter()
   assertResult(t, got, want)
 	
 }
 
+func TestAreaCircle(t *testing.T) {
+  want := 314.00
+	got := Circle{radius:10}.Area()
+  assertResult(t, got, want)
+	
+}
+
+func TestPerimeterCircle(t *testing.T) {
+	want := 62.00
+	got := Circle{radius:10}.Perimeter()
+  assertResult(t, got, want)
+	
+}
 
 func assertResult(t testing.TB, got, want float64) {
 	t.Helper()
